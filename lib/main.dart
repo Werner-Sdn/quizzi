180925920d5d7fff8fd64300a037cb6fee3cc7b6
wern <wern.dev@gmail.com>
2020-04-04 20:26:39 +0200

    first skeleton draft

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizzler/quiz_brain.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,8 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  QuizBrain quizBrain = QuizBrain();
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +40,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                quizBrain.questions[quizBrain.questionIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +64,13 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                quizBrain.questions[quizBrain.questionIndex].questionAnswer ==
+                        true
+                    ? quizBrain.results.add(quizBrain.correctAnswer())
+                    : quizBrain.results.add(quizBrain.wrongAnswer());
+                setState(() {
+                  quizBrain.displayQuestion();
+                });
               },
             ),
           ),
@@ -79,19 +88,23 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                quizBrain.questions[quizBrain.questionIndex].questionAnswer ==
+                        false
+                    ? quizBrain.results.add(quizBrain.correctAnswer())
+                    : quizBrain.results.add(quizBrain.wrongAnswer());
+                setState(() {
+                  quizBrain.displayQuestion();
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Expanded(
+          child: Row(
+            children: quizBrain.results,
+          ),
+        ),
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
---
+++ lib/quiz_brain.dart
@@ -0,0 +1,81 @@
+import 'question.dart';
+import 'package:flutter/material.dart';
+
+class QuizBrain {
+  int questionIndex = 0;
+  List<Icon> results = [];
+
+  int displayQuestion() {
+    return questionIndex < questions.indexOf(questions.last)
+        ? questionIndex++
+        : questionIndex = 0;
+  }
+
+  Icon correctAnswer() {
+    return Icon(
+      Icons.check,
+      color: Colors.green,
+    );
+  }
+
+  Icon wrongAnswer() {
+    return Icon(
+      Icons.block,
+      color: Colors.red,
+    );
+  }
+
+  List<Question> questions = [
+    Question(
+      'Some cats are actually allergic to humans',
+      true,
+    ),
+    Question(
+      'You can lead a cow down stairs but not up stairs.',
+      false,
+    ),
+    Question(
+      'Approximately one quarter of human bones are in the feet.',
+      true,
+    ),
+    Question(
+      'A slug\'s blood is green.',
+      true,
+    ),
+    Question(
+      'Buzz Aldrin\'s mother\'s maiden name was \"Moon\".',
+      true,
+    ),
+    Question(
+      'It is illegal to pee in the Ocean in Portugal.',
+      true,
+    ),
+    Question(
+      'No piece of square dry paper can be folded in half more than 7 times.',
+      false,
+    ),
+    Question(
+      'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+      true,
+    ),
+    Question(
+        'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+        false),
+    Question(
+      'The total surface area of two human lungs is approximately 70 square metres.',
+      true,
+    ),
+    Question(
+      'Google was originally called \"Backrub\".',
+      true,
+    ),
+    Question(
+      'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+      true,
+    ),
+    Question(
+      'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+      true,
+    ),
+  ];
+}
